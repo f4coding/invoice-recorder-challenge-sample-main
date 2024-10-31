6f8c753170c6be86466d2c4413ca4a1edd424e63
f4coding <fs.f4coding@gmail.com>
2024-10-31 17:39:52 -0500

    feature/eliminar-comprobante

--- app/Http/Controllers/Vouchers/Voucher/DeleteVoucherHandler.php
+++ app/Http/Controllers/Vouchers/Voucher/DeleteVoucherHandler.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Controllers\Vouchers\Voucher;
 
+use App\Models\Voucher;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
 class DeleteVoucherHandler
 {
+    public function __invoke(Request $request, string $id): JsonResponse
+    {
+        $voucher = Voucher::find($id);
 
+        if (!$voucher) {
+            return response()->json(['message' => 'Voucher not found'], 404);
+        }
+
+        $voucher->delete();
+
+        return response()->json([
+            'message' => 'Voucher deleted successfully',
+            'voucher' => $voucher
+        ], 200);
+    }
 }
